aeb483cd58b38628f24f16d50d46f6cd338c8534
Igorft3 <igor.sukharev.ft3@outlook.com>
2026-04-05 21:52:43 +0300

    feature: добавил обработу файлов и функцию для обработки запросов с сервера

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react'
+
+import axios from 'axios'
+
 import './App.css'
 
-function App() {
+const API_URL = 'http://loc2alhost:3000/'
 
-  return (
-    <>
-      <h1>Hello</h1>
-      </>
-  )
+function App() {
+    const [data, setData] = useState<string>('')
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        axios
+            .get(API_URL)
+            .then((response) => setData(response.data))
+            .catch((error) => setError(error.message))
+    }, [])
+
+    return (
+        <>
+            <h1>Hello</h1>
+            <p>
+                Fetch data: {data && data} {error && error}
+            </p>
+        </>
+    )
 }
 
 export default App
